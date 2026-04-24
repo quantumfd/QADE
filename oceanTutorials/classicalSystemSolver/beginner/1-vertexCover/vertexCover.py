39e28942d8cac058c434748e2b476061f27bdb5a
QuantumFD <contact@quantumfd.com>
2026-04-24 20:48:05 +0200

    Delete oceanTutorials directory

--- oceanTutorials/classicalSystemSolver/beginner/1-vertexCover/vertexCover.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-s5 = nx.star_graph(4)
-
-from dimod.reference.samplers import ExactSolver
-sampler = ExactSolver()
-
-import dwave_networkx as dnx 
-print(dnx.min_vertex_cover(s5, sampler))
